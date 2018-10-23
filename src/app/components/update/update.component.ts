a475367ec9c6a59118861489d9ff541e4898d4ca
Brian Hamlin <Brian.Hamlin@odfl.com>
2018-10-23 11:44:41 -0400

    Add update function

--- src/app/components/update/update.component.ts
+++ src/app/components/update/update.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { IssueService } from '../../services/issue.service';
-import { Issue } from '../../models/issue.model';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { IssueService } from "../../services/issue.service";
+import { Issue } from "../../models/issue.model";
+import { Router } from "@angular/router";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-update',
-  templateUrl: './update.component.html',
-  styleUrls: ['./update.component.css']
+  selector: "app-update",
+  templateUrl: "./update.component.html",
+  styleUrls: ["./update.component.css"]
 })
 export class UpdateComponent implements OnInit {
-  constructor(private issueService: IssueService, private router: Router) { 
-    if(issueService.issue == undefined){
-      router.navigate(['search']);
+  private currentIssue: Issue;
+  constructor(private issueService: IssueService, private router: Router) {
+    if (issueService.issue == undefined) {
+      router.navigate(["search"]);
+    } else {
+      this.currentIssue = issueService.issue;
     }
   }
 
-  ngOnInit() {
-  }
+  updateIssueForm = new FormGroup({
+    description: new FormControl("", Validators.required)
+  });
 
+  ngOnInit() {}
+
+  onSubmit() {
+    let updatedIssue = this.currentIssue;
+    updatedIssue.description = this.updateIssueForm.get("description").value;
+
+    this.issueService.updateIssue(updatedIssue).subscribe(() => {
+      this.router.navigate(["search"]);
+    });
+  }
 }
--- src/app/services/issue.service.ts
+++ src/app/services/issue.service.ts
@@ -1,26 +1,31 @@
-import { Injectable } from '@angular/core';
-import { Issue } from '../models/issue.model';
-import { AddIssueDialogComponent } from '../components/add-issue-dialog/add-issue-dialog.component';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { Issue } from "../models/issue.model";
+import { AddIssueDialogComponent } from "../components/add-issue-dialog/add-issue-dialog.component";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class IssueService {
   issue: Issue;
   issues: Observable<any>;
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient) {}
 
-  addIssue(issue: Issue){
-    return this.http.post<Issue>('http://localhost:3000/issues', issue);
+  addIssue(issue: Issue) {
+    return this.http.post<Issue>("http://localhost:3000/issues", issue);
   }
-  deleteIssue(issueId){
-    return this.http.delete<Issue>('http://localhost:3000/issues/' + issueId);
+  deleteIssue(issueId) {
+    return this.http.delete<Issue>("http://localhost:3000/issues/" + issueId);
   }
-   getIssues(){
-     return this.http.get('http://localhost:3000/issues');  
- }
+  getIssues() {
+    return this.http.get("http://localhost:3000/issues");
+  }
 
+  updateIssue(updatedIssue: Issue) {
+    return this.http.patch<Issue>(
+      "http://localhost:3000/issues/" + updatedIssue.id,
+      updatedIssue
+    );
+  }
 }
-
