27f962d89d16a059c61c55e469aa812462c21e0b
Brian Hamlin <e076236@Brians-MacBook-Pro.local>
2018-11-27 11:13:34 -0500

    Move host to class constant and use template literal

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { SearchComponent } from "../components/search/search.component";
-import { UpdateComponent } from "../components/update/update.component";
-import { SignupComponent } from "../components/auth/signup/signup.component";
-import { LoginComponent } from "../components/auth/login/login.component";
-import { AuthGuard } from "../components/auth/auth-guard.service";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from '../components/search/search.component';
+import { UpdateComponent } from '../components/update/update.component';
+import { SignupComponent } from '../components/auth/signup/signup.component';
+import { LoginComponent } from '../components/auth/login/login.component';
+import { AuthGuard } from '../components/auth/auth-guard.service';
 
 const routes: Routes = [
-  { path: "", canActivate: [AuthGuard], component: SearchComponent },
-  { path: "search", canActivate: [AuthGuard], component: SearchComponent },
-  { path: "update", canActivate: [AuthGuard], component: UpdateComponent },
-  { path: "login", component: LoginComponent },
-  { path: "signup", component: SignupComponent }
+  { path: '', canActivate: [AuthGuard], component: SearchComponent },
+  { path: 'search', canActivate: [AuthGuard], component: SearchComponent },
+  { path: 'update', canActivate: [AuthGuard], component: UpdateComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
--- src/app/datasource/rest.datasource.ts
+++ src/app/datasource/rest.datasource.ts
@@ -4,30 +4,31 @@ import { Project } from '../models/project.model';
 import { Observable } from 'rxjs';
 import { Issue } from '../models/issue.model';
 
+const HOST = 'http://localhost:3000';
+
 @Injectable()
 export class RestDataSource {
   constructor(private httpClient: HttpClient) {}
-
   getProjects(): Observable<Project[]> {
-    return this.httpClient.get<Project[]>('http://localhost:3000/projects');
+    return this.httpClient.get<Project[]>(`${HOST}/projects`);
   }
 
   getIssues(): Observable<Issue[]> {
-    return this.httpClient.get<Issue[]>('http://localhost:3000/issues');
+    return this.httpClient.get<Issue[]>(`${HOST}/issues`);
   }
 
   addIssue(issue: Issue) {
-    return this.httpClient.post<Issue>('http://localhost:3000/issues', issue);
+    return this.httpClient.post<Issue>(`${HOST}/issues`, issue);
   }
 
   updateIssue(updatedIssue: Issue) {
     return this.httpClient.patch<Issue>(
-      'http://localhost:3000/issues/' + updatedIssue.id,
+      `${HOST}/issues/${updatedIssue.id}`,
       updatedIssue
     );
   }
 
   deleteIssue(issueId) {
-    return this.httpClient.delete('http://localhost:3000/issues/' + issueId);
+    return this.httpClient.delete(`${HOST}/issues/${issueId}`);
   }
 }
